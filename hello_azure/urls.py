cd0dceaece7a71732755d1ba70e7c29312486def
Gao Zheng <62479296+gaogaogao777@users.noreply.github.com>
2023-12-08 04:48:11 +0800

    Update urls.py

--- hello_azure/urls.py
+++ hello_azure/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('hello', views.hello, name='hello'),
     path('', views.name, name='name'),
     path('reviews', views.show_reviews, name='show_reviews'),
 
